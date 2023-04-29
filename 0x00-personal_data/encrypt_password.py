48cde5ae9d188c7aba0663138ebe6241fbbee187
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-29 21:34:47 +0100

    encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -17,3 +17,18 @@ from bcrypt import hashpw
 def hash_password(password: str) -> bytes:
     """ Encrypts a password using bcrypt """
     return hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+"""
+    Implement an is_valid function that expects 2 arguments and returns a
+    boolean.
+
+    Arguments:
+        hashed_password: bytes type
+        password: string type
+"""
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ ensures the provided password matches the hashed password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
