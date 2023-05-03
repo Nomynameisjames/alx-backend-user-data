04eb6144045bd45295c3f7433aa9750248180df1
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-03 02:00:10 +0100

    decode basicAuth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+     
+    def decode_base64_authorization_header(
+             self, base64_auth_header: str) -> str:
+        """
+            Returns decoded value of a Base64 string
+        """
+        if base64_auth_header is None:
+            return None
+        if not isinstance(base64_auth_header, str):
+            return None
+        try:
+            decode = base64.b64decode(base64_auth_header.encode('utf-8')
+                                      ).decode('utf-8')
+            return decode
+        except Exception:
+            return None
