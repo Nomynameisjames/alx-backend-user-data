908585f47548e60c4e32fdc6733a82ce2dac2151
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-03 01:50:26 +0100

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,9 +6,19 @@ import base64
 from .auth import Auth
 from typing import TypeVar
 
-from models.user import User
-
 
 class BasicAuth(Auth):
     """BasicAuth class"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
+        """
+            Returns Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
