04e07b0ec24753599459bdccd0d68fd7e5a4735b
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-03 01:44:46 +0100

    basicauth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    import files
+"""
+import base64
+from .auth import Auth
+from typing import TypeVar
+
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """BasicAuth class"""
+    pass
