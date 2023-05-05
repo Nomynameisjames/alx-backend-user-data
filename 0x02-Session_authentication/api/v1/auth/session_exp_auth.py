26704739c279f760af26164cf7731feca2eea08e
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-05 03:38:34 +0100

    timed session

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+    import files
+"""
+import os
+from datetime import datetime, timedelta
+from api.v1.auth.session_auth import SessionAuth
+
+
+class SessionExpAuth(SessionAuth):
+    """
+        SessionExpAuth class
+    """
+    def __init__(self):
+        try:
+            self.session_duration = int(os.getenv('SESSION_DURATION'))
+        except Exception:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """
+            create session
+        """
+        if user_id is None:
+            return None
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dictionary = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+            user id for session id
+        """
+        if session_id is None:
+            return None
+        if session_id not in self.user_id_by_session_id:
+            return None
+        if "created_at" not in self.user_id_by_session_id[session_id]:
+            return None
+        if self.session_duration <= 0:
+            return self.user_id_by_session_id[session_id]["user_id"]
+        created_at = self.user_id_by_session_id[session_id]["created_at"]
+        expired_time = created_at + timedelta(seconds=self.session_duration)
+        if expired_time < datetime.now():
+            return None
+        return self.user_id_by_session_id[session_id]["user_id"]
