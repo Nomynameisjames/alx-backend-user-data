ac7d3366636b8e70d6399ce99ad830447000649e
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-05 02:19:51 +0100

    implemented user session cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,11 +36,11 @@ def before_request() -> str:
                       '/api/v1/forbidden/',
                       '/api/v1/auth_session/login/'
                       ]
-    if auth.authorization_header(request) and\
-            auth.session_cookie(request) is None:
-        abort(401, 'Unauthorized')
     if not auth.require_auth(request.path, excluded_paths):
         return
+    if auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
+        abort(401, 'Unauthorized')
     if auth.authorization_header(request) is None:
         abort(401, 'Unauthorized')
     if auth.current_user(request) is None:
