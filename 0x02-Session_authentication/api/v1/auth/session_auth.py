1e365213f89f3419ff4b1c4da527ce0ee2e82f31
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-05 01:31:01 +0100

    sessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""import files"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """
+        SessionAuth class
+    """
+    pass
