ec91b2c7a21b3c8c24117b73321990d5babcf742
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-05 02:24:15 +0100

    implemented  Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -33,3 +33,11 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request: str = None) -> str:
+        """
+            current user
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
