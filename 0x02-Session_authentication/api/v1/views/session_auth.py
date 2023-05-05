4984e7b324fb6638ec89ecad46ea23910b30de3d
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-05 02:46:34 +0100

    end of session

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -36,3 +36,16 @@ def login():
         response = jsonify(user.to_json())
         response.set_cookie(cookie_name, cookie_value, path=cookie_path)
         return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """
+        user logout logic implementation
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    else:
+        return jsonify({}), 200
