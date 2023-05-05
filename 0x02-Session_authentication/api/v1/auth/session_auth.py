88ae13cf8cb77e36288bb79f30271eea942a49a7
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-05 02:00:15 +0100

    implemented user session cookie

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -40,5 +40,5 @@ class SessionAuth(Auth):
         """
         if request is None:
             return None
-        session_name = os.getenv("SESSION_NAME")
-        return request.cookies.get(session_name)
+        _my_session_id = os.getenv("SESSION_NAME")
+        return request.cookies.get(_my_session_id)
